4a3cb223967e6cc6867f50e1eb793bc7b06057ea
Mark Emery <mark.e.emery@gmail.com>
2022-11-18 20:33:30 -0500

    Update animate.js

--- js/animate.js
+++ js/animate.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
     // SPOTIFY
         $( "#button-s" ).hoverIntent(function() {
-            $("#shadow-s").animate({width: '15.7vw'}, 250);
+            $("#shadow-s").animate({width: '21vw'}, 250);
         },
         function(){
             $("#shadow-s").animate({width: '1vw'}, 250);
@@ -77,7 +77,7 @@ $(document).ready(function(){
 
     // RESUME
         $( "#button-r" ).hoverIntent(function() {
-            $("#shadow-r").animate({width: '15.7vw'}, 250);
+            $("#shadow-r").animate({width: '21vw'}, 250);
         },
         function(){
             $("#shadow-r").animate({width: '1vw'}, 250);
